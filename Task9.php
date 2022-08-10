c8d159c32e6b315f01cc152b13dc963dc607e1e2
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:23:58 +0300

    Update Task9.php

---
+++ Task9.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+class Task9
+{
+    public static function main(array $arr, int $number)
+    {
+        $count = count($arr) - 2;
+        $result = [];
+        for ($i = 0; $i < $count; $i++) {
+            if ($arr[$i] + $arr[$i+1] + $arr[$i+2] === $number) {
+                array_push($result, "{$arr[$i]} + {$arr[$i+1]} + {$arr[$i+2]} = $number");
+            }
+        }
+        return $result;
+    }
+}
