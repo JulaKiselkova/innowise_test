8cbf32bd532d9159fa20eb9489ce3eec2ddec6ad
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:13:33 +0300

    Update Task8.php

---
+++ Task8.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace src;
+
+class Task8
+{
+    public static function main(string $json)
+    {
+        $obj = json_decode($json, true);
+        $arrOfStrings = [];
+        foreach ($obj as $key => $value) {
+            if (gettype($value)!="array") {
+                array_push($arrOfStrings, "{$key}: {$value}\n");
+            } else {
+                foreach ($value as $keyInside => $valueInside) {
+                    array_push($arrOfStrings, "{$keyInside}: {$valueInside}\n");
+                }
+            }
+        }
+        return implode("", $arrOfStrings);
+    }
+}
