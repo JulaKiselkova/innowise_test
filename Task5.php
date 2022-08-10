8e7027486205185c934457ac181d5b2a45b646ee
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:09:21 +0300

    Update Task5.php

---
+++ Task5.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace src;
+
+class Task5
+{
+    public static function main(int $n)
+    {
+        if ($n < 0) {
+            throw new \InvalidArgumentException("Number must be positive");
+        }
+        if ($n <= 1) {
+            return $n;
+        }
+        return fib($n - 1) +
+            fib($n - 2);
+    }
+}
