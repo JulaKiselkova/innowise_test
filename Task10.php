08f7d735c33cff1cf8fd7dacca42e66149042da9
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:24:32 +0300

    Update Task10.php

---
+++ Task10.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace src;
+
+class Task10
+{
+    public static function main(int $input)
+    {
+        if ($input === 1) {
+            return 1;
+        } elseif ($input % 2 === 0) {
+            $newNumber = $input/2;
+            return collatz($newNumber);
+        } else {
+            $newNumber = ($input*3)+1;
+            return collatz($newNumber);
+        }
+    }
+}
