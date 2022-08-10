7b664282ae7294fb20169e95490f3d949d747445
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:19:08 +0300

    Update Task4.php

---
+++ Task4.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace src;
+
+class Task1
+{
+    public static function main($input)
+    {
+        if (gettype($input) != 'string') {
+            throw new \InvalidArgumentException('Wrong data');
+        } else {
+            $results = preg_split('/[\s_-]+/', $input);
+            $arrOfResult = [];
+            foreach ($results as $res) {
+                $res[0] = strtoupper($res);
+                array_push($arrOfResult, $res);
+            }
+
+            return implode('', $arrOfResult);
+        }
+    }
+}
