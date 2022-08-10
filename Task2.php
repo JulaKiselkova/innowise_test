14134313c40d805447cb410aba6040032541f15b
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-10 17:02:07 +0300

    Update Task2.php

---
+++ Task2.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src;
+
+class Task2
+{
+    public static function main(string $date)
+    {
+        if (strtotime($date) === false) {
+            throw new \InvalidArgumentException("Wrong data");
+        } else {
+            $today = date("d.m.Y");
+            $d1_ts = strtotime($today);
+            $d2_ts = strtotime($date);
+            $seconds = abs($d1_ts - $d2_ts);
+            $days = $seconds/(60*60*24);
+            return $days;
+        }
+    }
+}
