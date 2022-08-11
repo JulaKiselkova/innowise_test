d86b753c091e65b696a0255f22704cf28fa38f7b
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-11 17:11:19 +0300

    2

--- Task8.php
+++ Task8.php
@@ -9,7 +9,7 @@ class Task8
         $obj = json_decode($json, true);
         $arrOfStrings = [];
         foreach ($obj as $key => $value) {
-            if (gettype($value)!="array") {
+            if (gettype($value) != 'array') {
                 array_push($arrOfStrings, "{$key}: {$value}\n");
             } else {
                 foreach ($value as $keyInside => $valueInside) {
@@ -17,6 +17,7 @@ class Task8
                 }
             }
         }
-        return implode("", $arrOfStrings);
+
+        return implode('', $arrOfStrings);
     }
 }
