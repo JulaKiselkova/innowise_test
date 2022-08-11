7f4bdbf7cfac73700a5e3ada0c4ce02009543f79
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-11 14:45:16 +0300

    2

--- Task1.php
+++ Task1.php
@@ -4,15 +4,16 @@ namespace src;
 
 class Task1
 {
-    public static function main($n)
+    public static function main(int $n)
     {
         $result = $n > 30
-            ? "More than 30"
+            ? 'More than 30'
             : ($n > 20
-                ? "More than 20"
-                : ($n >10
-                    ? "More than 10"
-                    : "Equal or less than 10"));
+                ? 'More than 20'
+                : ($n > 10
+                    ? 'More than 10'
+                    : 'Equal or less than 10'));
+
         return $result;
     }
 }
