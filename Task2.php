f9e31750ab6906e506f8ca593f32a24af21af897
Yulia Kiselkova <78469979+JulaKiselkova@users.noreply.github.com>
2022-08-11 16:00:56 +0300

    Update Task2.php

--- Task2.php
+++ Task2.php
@@ -7,13 +7,14 @@ class Task2
     public static function main(string $date)
     {
         if (strtotime($date) === false) {
-            throw new \InvalidArgumentException("Wrong data");
+            throw new \InvalidArgumentException('Wrong data');
         } else {
-            $today = date("d.m.Y");
+            $today = date('d.m.Y');
             $d1_ts = strtotime($today);
             $d2_ts = strtotime($date);
             $seconds = abs($d1_ts - $d2_ts);
-            $days = $seconds/(60*60*24);
+            $days = $seconds / (60 * 60 * 24);
+
             return $days;
         }
     }
