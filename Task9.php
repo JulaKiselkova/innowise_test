03db21cd389e094cd96c5e412d1837bd2f9ce7e2
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-11 17:12:20 +0300

    2

--- Task9.php
+++ Task9.php
@@ -9,10 +9,11 @@ class Task9
         $count = count($arr) - 2;
         $result = [];
         for ($i = 0; $i < $count; $i++) {
-            if ($arr[$i] + $arr[$i+1] + $arr[$i+2] === $number) {
-                array_push($result, "{$arr[$i]} + {$arr[$i+1]} + {$arr[$i+2]} = $number");
+            if ($arr[$i] + $arr[$i + 1] + $arr[$i + 2] === $number) {
+                array_push($result, "{$arr[$i]} + {$arr[$i + 1]} + {$arr[$i + 2]} = $number");
             }
         }
+
         return $result;
     }
 }
