48d1897c9547e6cc7101bef9335616afa3b4068a
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 08:28:26 +0300

    fix

--- Task10.php
+++ Task10.php
@@ -4,18 +4,32 @@ namespace src;
 
 class Task10
 {
-    public static function main(int $input)
+    public static function collatz(int $input, $arrOfRes)
     {
+        if ($input <= 0) {
+            throw new \InvalidArgumentException('incorrect data');
+        }
         if ($input === 1) {
-            return 1;
+            array_push($arrOfRes, 1);
+
+            return $arrOfRes;
         } elseif ($input % 2 === 0) {
             $newNumber = $input / 2;
+            echo "${newNumber},";
+            array_push($arrOfRes, $input);
 
-            return collatz($newNumber);
+            return Task10::collatz($newNumber, $arrOfRes);
         } else {
             $newNumber = ($input * 3) + 1;
+            echo "${newNumber},";
+            array_push($arrOfRes, $input);
 
-            return collatz($newNumber);
+            return Task10::collatz($newNumber, $arrOfRes);
         }
+    }
+
+    public static function main(int $input)
+    {
+        return self::collatz($input, []);
     }
 }
