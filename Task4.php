6bdb025c68c9d18e4467107e9d8c789c29c6a34c
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 10:50:12 +0300

    fix

--- Task4.php
+++ Task4.php
@@ -4,19 +4,10 @@ namespace src;
 
 class Task4
 {
-    public static function main(int $input)
+    public function main(string $input)
     {
-        if (gettype($input) != 'string') {
-            throw new \InvalidArgumentException('Wrong data');
-        } else {
-            $results = preg_split('/[\s_-]+/', $input);
-            $arrOfResult = [];
-            foreach ($results as $res) {
-                $res[0] = strtoupper($res);
-                array_push($arrOfResult, $res);
-            }
+        $wordsToUpper = ucwords($input, '-_ ');
 
-            return implode('', $arrOfResult);
-        }
+        return str_replace(['-', '_', ' '], '', $wordsToUpper);
     }
 }
