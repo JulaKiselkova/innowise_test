e7e2b7ad771b7c75f8099ebb7749a67aaf00cfd3
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 10:48:30 +0300

    fix

--- Task3.php
+++ Task3.php
@@ -6,8 +6,10 @@ class Task3
 {
     public static function main(int $n)
     {
-        if ($n < 0 || gettype($n) === 'float') {
+        if ($n < 0) {
             throw new \InvalidArgumentException('Number must be positive');
+        } elseif (!is_int($n)) {
+            throw new \InvalidArgumentException('Number must be int');
         } elseif ($n < 10) {
             return $n;
         } else {
