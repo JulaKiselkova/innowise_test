047071e7482ed0246b09093733160da4bf82e4d4
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 13:31:42 +0300

    fix

--- Task2.php
+++ Task2.php
@@ -12,11 +12,21 @@ class Task2
         $now = date_create(date('d-m-Y'));
         $birthday = date_create($date);
         $diff = date_diff($birthday, $now);
-        if ($birthday > $now) {
-            return $diff->days;
-        } else {
-            return '-' . $diff->days;
-        }
+
+        $day = date('d', $birthday->getTimestamp());
+        $month = date('m', $birthday->getTimestamp());
+        $year = date('Y', $now->getTimestamp());
+
+        $date = "${day}-${month}-${year}";
+        $birthdayThisYear = date_create_from_format('d-m-Y', $date);
+
+        $nextYear = (int)$year + 1;
+        $dateDiff = date_diff($now, $birthdayThisYear);
+
+        $dateToCompare = $dateDiff->invert < 1 ?
+            $birthdayThisYear : date_create_from_format('d-m-Y', "${day}-${month}-${nextYear}");
+
+        return date_diff($dateToCompare, $now)->days;
     }
 
     public static function validDate(string $date): bool
@@ -26,3 +36,5 @@ class Task2
         return $dateObj && $dateObj->format('d-m-Y') === $date;
     }
 }
+
+echo Task2::main('12-08-2024');
