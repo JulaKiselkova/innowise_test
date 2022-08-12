491824c3941466bf67266cb4144fbb8de6a8e891
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 11:15:21 +0300

    fix

--- Task8.php
+++ Task8.php
@@ -4,20 +4,35 @@ namespace src;
 
 class Task8
 {
-    public static function main(string $json)
+    public function main(string $json)
     {
-        $obj = json_decode($json, true);
-        $arrOfStrings = [];
-        foreach ($obj as $key => $value) {
-            if (gettype($value) != 'array') {
-                array_push($arrOfStrings, "{$key}: {$value}\n");
+        $data = json_decode($json, true);
+        if (!(is_array($data) && json_last_error() === JSON_ERROR_NONE)) {
+            throw new \InvalidArgumentException('valid json');
+        }
+        $result = $this->toUnnestedArray($data);
+        foreach ($result as $key => $value) {
+            echo $key . ': ' . $value . "\n";
+        }
+    }
+
+    public function toUnnestedArray(array $array): array
+    {
+        if (!is_array($array)) {
+            throw new \InvalidArgumentException('array only');
+        }
+        if (empty($array)) {
+            throw new \InvalidArgumentException('function accepts empty array as argument');
+        }
+        $result = [];
+        foreach ($array as $key => $value) {
+            if (!is_array($value)) {
+                $result[$key] = $value;
             } else {
-                foreach ($value as $keyInside => $valueInside) {
-                    array_push($arrOfStrings, "{$keyInside}: {$valueInside}\n");
-                }
+                $result = array_merge($result, $this->toUnnestedArray($value));
             }
         }
 
-        return implode('', $arrOfStrings);
+        return $result;
     }
 }
