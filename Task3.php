e65afda2d8b81d7e760df31d4d136e44d07b7f2a
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 13:37:55 +0300

    task-3 commit

--- Task3.php
+++ Task3.php
@@ -4,11 +4,16 @@ namespace src;
 
 class Task3
 {
-    public static function main(int $n)
+    public static function main($n)
     {
-        if ($n <= 1 || gettype($n) === 'float') {
+        echo gettype($n);
+        echo(is_float($n));
+        if ($n <= 1) {
             throw new \InvalidArgumentException('Number must be positive');
-        } elseif ($n < 10) {
+        } elseif (is_float($n)) {
+            throw new \InvalidArgumentException('float');
+        }
+        if ($n < 10) {
             return $n;
         } else {
             return ($n - 1) % 9 + 1;
