d990aa70494aaee15253560e7dbc12dc3e47126f
JulaKiselkova <Yulia.kiselkova@mail.ru>
2022-08-12 08:44:18 +0300

    fix

--- Task9.php
+++ Task9.php
@@ -6,6 +6,12 @@ class Task9
 {
     public static function main(array $arr, int $number)
     {
+        foreach ($arr as $elem) {
+            if ($elem !== (int) $elem) {
+                throw new \InvalidArgumentException('first function argument should be an array of numbers only');
+            }
+        }
+
         $count = count($arr) - 2;
         $result = [];
         for ($i = 0; $i < $count; $i++) {
